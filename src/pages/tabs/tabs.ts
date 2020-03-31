58bd0bce83af09e7f44bbda97c7490d7bba86521
Nittaya Puk <somz28747@gmail.com>
2020-03-31 22:29:12 +0700

    first commit

---
+++ src/pages/tabs/tabs.ts
@@ -0,0 +1,24 @@
+import { Component } from '@angular/core';
+
+import { AboutPage } from '../about/about';
+import { ContactPage } from '../contact/contact';
+import { HomePage } from '../home/home';
+import { HotnewsPage } from '../hotnews/hotnews';
+import { SciencePage } from '../science/science';
+import { EntertainmentPage } from '../entertainment/entertainment';
+import { DetailPage } from '../detail/detail';
+
+
+@Component({
+  templateUrl: 'tabs.html'
+})
+export class TabsPage {
+
+  tab1Root = HotnewsPage;
+  tab2Root = SciencePage;
+  tab3Root = EntertainmentPage;
+
+  constructor() {
+
+  }
+}
